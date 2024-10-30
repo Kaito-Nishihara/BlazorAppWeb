a66a891963a890d4b255c1ae3c2ab765f886631d
Kaito Nishihara <78912762+Kaito-Nishihara@users.noreply.github.com>
2024-10-30 09:05:01 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorAppWeb/Identity/Models/FormResult.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorAppWeb.Identity.Models
+{
+    public class FormResult
+    {
+        public bool Succeeded { get; set; }
+        public string[] ErrorList { get; set; } = [];
+    }
+}
